458926f0a5e7b556e30b4f13ada4914574d6ec0e
Tuomas Suutari <tuomas@nepnep.net>
2022-11-14 13:32:55 +0200

    Ikoni ja index.tsx&_app.tsx uusiksi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/globals.css';
 import type {AppProps} from 'next/app';
+import Head from 'next/head';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import '../styles/globals.css';
 import Layout from '@/components/layout';
 
 export default function App({Component, pageProps}: AppProps) {
     return (
         <Layout>
+            <Head>
+                <title>Kauppalista</title>
+                <meta
+                    name="description"
+                    content="Kauppalistan hallintaohjelma"
+                />
+                <link rel="icon" href="/kauppalista.ico" />
+            </Head>
+
             <Component {...pageProps} />
         </Layout>
     );
