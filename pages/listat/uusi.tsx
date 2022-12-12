2b6280c606c33008a084be21a1fac45f8ab49be7
Tuomas Suutari <tuomas@nepnep.net>
2022-12-12 14:02:55 +0200

    Uuden listan tekeminen API-kutsulla

--- pages/listat/uusi.tsx
+++ pages/listat/uusi.tsx
@@ -1,30 +1,57 @@
-import {FormEvent, useRef} from 'react';
-import {Button, Form} from 'react-bootstrap';
+import { useRouter } from 'next/router';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
-import {Page} from '@/components/Page';
+import { Page } from '@/components/Page';
+import { callApi } from '@/utils/apicall';
 
 export default function UusiListaSivu() {
-    const nameInput = useRef<HTMLInputElement>(null);
+    const [name, setName] = useState<string>('');
+    const [nameIsInvalid, setNameIsInvalid] = useState<boolean>(false);
+    const router = useRouter();
 
-    function handleSubmit(event: FormEvent<HTMLFormElement>): void {
+    function handleNameChange(event: ChangeEvent<HTMLInputElement>) {
+        const name = event.target.value;
+        setName(name);
+        if (name) setNameIsInvalid(false);
+        else setNameIsInvalid(true);
+    }
+
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        if (!nameInput.current) return;
-        const nimi = nameInput.current.value;
 
-        // TODO: Kutsu APIa
+        if (!name) {
+            setNameIsInvalid(true);
+            return;
+        }
+
+        const response = await callApi('/api/listat/', 'POST', {name});
+        if (response && response?.id) {
+            router.push(`/lista/${response.id}`);
+        } else if (response && response.error) {
+            alert(`Virhe: ${response.error}`);
+        } else {
+            console.error('Error from API', response);
+            alert(`Virhe: ${JSON.stringify(response)}`);
+        }
     }
 
     return (
         <Page title="Luo uusi kauppalista">
-            <Form onSubmit={handleSubmit}>
+            <Form
+                onSubmit={handleSubmit}
+                className={nameIsInvalid ? 'was-validated' : undefined}
+            >
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Nimi</Form.Label>
+                    <Form.Label for="nimi">Nimi</Form.Label>
                     <Form.Control
-                        name="nimi"
+                        id="nimi"
                         type="text"
+                        required
                         placeholder="Anna kauppalistalle nimi"
-                        ref={nameInput}
+                        onChange={handleNameChange}
                     />
+                    <div className={'invalid-feedback'}>Nimi vaaditaan</div>
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Luo
--- utils/apicall.ts
+++ utils/apicall.ts
@@ -1,5 +1,19 @@
-export async function callApi(url: string): Promise<any> {
-    const baseUrl = process.env.BASE_URL;
-    const response = await fetch(`${baseUrl}${url}`);
+type HttpMethod = 'GET' | 'POST' | 'PATCH' | 'DELETE';
+
+const defaultHeaders: HeadersInit = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+};
+
+export async function callApi(
+    url: string,
+    method: HttpMethod = 'GET',
+    data: any = undefined,
+    headers: HeadersInit = defaultHeaders,
+): Promise<any> {
+    const baseUrl = process.env.BASE_URL ?? 'http://localhost:3000';
+    const body = data !== undefined ? JSON.stringify(data) : undefined;
+    const request = new Request(`${baseUrl}${url}`, {method, headers, body});
+    const response = await fetch(request);
     return await response.json();
 }
