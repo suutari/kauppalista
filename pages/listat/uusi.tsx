193c063044738014957f789e310b5223927d0f67
Tuomas Suutari <tuomas@nepnep.net>
2022-12-12 12:02:56 +0200

    Uusi lista -formi

--- pages/listat/uusi.tsx
+++ pages/listat/uusi.tsx
@@ -1,8 +1,35 @@
+import {FormEvent, useRef} from 'react';
+import {Button, Form} from 'react-bootstrap';
+
 import {Page} from '@/components/Page';
 
 export default function UusiListaSivu() {
+    const nameInput = useRef<HTMLInputElement>(null);
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>): void {
+        event.preventDefault();
+        if (!nameInput.current) return;
+        const nimi = nameInput.current.value;
+
+        // TODO: Kutsu APIa
+    }
+
     return (
         <Page title="Luo uusi kauppalista">
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Nimi</Form.Label>
+                    <Form.Control
+                        name="nimi"
+                        type="text"
+                        placeholder="Anna kauppalistalle nimi"
+                        ref={nameInput}
+                    />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Luo
+                </Button>
+            </Form>
         </Page>
     );
 }
